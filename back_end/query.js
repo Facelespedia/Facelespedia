b3b72d000da73e17970c72759f9de5201f3fb04c
GDROM <Rommadon.t@ku.th>
2017-04-19 14:51:53 +0700

    add path database

--- back_end/query.js
+++ back_end/query.js
@@ -5,22 +5,92 @@ var connection;
     host     : 'localhost',
     user     : 'root',
     database : 'facelespedia'
-  });                                            
+  });
 
-  connection.connect(function(err) {              
-    if(err) {                                     
+  connection.connect(function(err) {
+    if(err) {
       console.log('Error : ', err);
     }else {
       console.log('Database Connect');
-    }                                  
+    }
   });
 
-exports.getPersons = function(callback){
-  connection.query("SELECT * FROM Personss",function(err, results,fields){
-    if(!!err) console.log(err);
-    var data  = {
-      person : results
-    }
-    callback(data);
-  });
-}
+  exports.getPlayer = function(callback){
+    connection.query("SELECT * FROM Player",function(err, results,fields){
+      if(!!err) console.log(err);
+      var data  = {
+        Player : results
+      }
+      callback(data);
+    });
+  }
+
+  exports.getTeam = function(callback){
+    connection.query("SELECT * FROM Team",function(err, results,fields){
+      if(!!err) console.log(err);
+      var data  = {
+        Team : results
+      }
+      callback(data);
+    });
+  }
+
+  exports.getHero = function(callback){
+    connection.query("SELECT * FROM Hero",function(err, results,fields){
+      if(!!err) console.log(err);
+      var data  = {
+        Hero : results
+      }
+      callback(data);
+    });
+  }
+
+  exports.getHeroStat = function(callback){
+    connection.query("SELECT * FROM HeroStat",function(err, results,fields){
+      if(!!err) console.log(err);
+      var data  = {
+        person : results
+      }
+      callback(data);
+    });
+  }
+
+  exports.getContinent = function(callback){
+    connection.query("SELECT * FROM Continent",function(err, results,fields){
+      if(!!err) console.log(err);
+      var data  = {
+        person : results
+      }
+      callback(data);
+    });
+  }
+
+  exports.getContinentTeam = function(callback){
+    connection.query("SELECT * FROM ContinentTeam",function(err, results,fields){
+      if(!!err) console.log(err);
+      var data  = {
+        person : results
+      }
+      callback(data);
+    });
+  }
+
+  exports.getContinentPlayer = function(callback){
+    connection.query("SELECT * FROM ContinentPlayer",function(err, results,fields){
+      if(!!err) console.log(err);
+      var data  = {
+        person : results
+      }
+      callback(data);
+    });
+  }
+
+  exports.getTeamMember = function(callback){
+    connection.query("SELECT * FROM TeamMember",function(err, results,fields){
+      if(!!err) console.log(err);
+      var data  = {
+        person : results
+      }
+      callback(data);
+    });
+  }
