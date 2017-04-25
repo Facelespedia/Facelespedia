8bfde36b6ff78d27351e6766c438500e4fb861d7
Narongsak <narongsak.chobsri@gmail.com>
2017-04-26 02:15:23 +0700

    add all photo and modify function images

--- client.js
+++ client.js
@@ -6,6 +6,8 @@ var app = express();
 var cors = require('cors')
 var bodyParser = require('body-parser')
 var routes = require('./routes/index')
+var test = require('./routes/test')
+
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
@@ -13,6 +15,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname,'public')));
 app.use('/',routes);
+app.use('/test',test);
 
 app.listen(PORT , function(err) {
     if(!!err) {
